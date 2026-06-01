d1c50ed63face8b85bfdde05120a0c60d9ad93d0
User <user@example.com>
2026-06-01 12:16:09 -0500

    a

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let filteredData = [];
 let currentChannel = '';
 
 // URL base de la API
-const API_BASE_URL = 'http://pdvxiaomi.railway.internal:3000/api/Xiaomi/pdv';
+const API_BASE_URL = 'https://pdvxiaomi-production.up.railway.app:8888/api/Xiaomi/pdv';
 
 // Cargar datos de la API según el canal seleccionado
 async function loadData() {
